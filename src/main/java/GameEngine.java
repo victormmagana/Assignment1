59ff30952e51ec5703497b449109958aac160180
EvaluateFile <plush.escapee2c@icloud.com>
2026-01-28 22:43:13 -0700

    fixed rebase conflicts with feature 3 and dev

--- src/main/java/GameEngine.java
+++ src/main/java/GameEngine.java
@@ -4,33 +4,25 @@ public class GameEngine {
     private int target;
     private int attempts;
     private boolean gameWon;
-    private boolean userQuit;
 
     public GameEngine(int min, int max) {
         this.min = min;
         this.max = max;
         this.attempts = 0;
         this.gameWon = false;
-        this.userQuit = false;
         reset();
     }
 
     public GuessResult makeGuess(int guess) {
-        // Check if user wants to quit (negative number)
-        if (guess < 0) {
-            userQuit = true;
-            return new GuessResult(false, "Exiting game...", attempts);
-        }
-
         attempts++;
 
         if (guess == target) {
             gameWon = true;
             return new GuessResult(true, "Correct! You guessed it in " + attempts + " attempts.", attempts);
         } else if (guess < target) {
-            return new GuessResult(false, "Too low! Try a higher number.", attempts);
+            return new GuessResult(false, "Too low!", attempts);
         } else {
-            return new GuessResult(false, "Too high! Try a lower number.", attempts);
+            return new GuessResult(false, "Too high!", attempts);
         }
     }
 
@@ -38,15 +30,10 @@ public class GameEngine {
         target = Utils.randomInt(min, max);
         attempts = 0;
         gameWon = false;
-        userQuit = false;
     }
 
     public boolean isGameWon() {
         return gameWon;
-    }
-
-    public boolean hasUserQuit() {
-        return userQuit;
     }
 
     public int getAttempts() {
